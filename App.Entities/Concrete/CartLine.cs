b7c318a7d17c485755505b4aa78b9d6481af8e27
Ahme_if16 <Ahme_if16@STHQ0122-04>
2023-01-24 20:35:07 +0400

    Add project files.

---
+++ App.Entities/Concrete/CartLine.cs
@@ -0,0 +1,15 @@
+﻿using App.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Entities.Concrete
+{
+    public class CartLine : IEntity
+    {
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+    }
+}
